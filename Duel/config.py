a8bebcc5e3c036e2a01cc5758ae9cb373edfd2b3
陈骐 <117030255+Kingcxp@users.noreply.github.com>
2022-10-31 00:11:56 +0800

    Add files via upload

---
+++ Duel/config.py
@@ -0,0 +1,21 @@
+class Config:
+
+    used_in_group = []#"714021174", "812023594"]
+
+    priority = 5
+
+    friend_bot = "3277105899"
+
+    friends = ["2371892518", "1960572173", "2503576899"]
+
+    bot_id = "3174143625"
+
+    bot_name = ["ABot", "abot", "Abot"]
+
+    super_uid = ["404291187", "1832783120"]
+
+    p_chat_response = 100
+
+    p_poke_response = 100
+
+    default_ban_time = 3600
